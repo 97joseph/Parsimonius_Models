e30fd9ea3457d50e7b94486889e513e241dc9844
97joseph <33089347+97joseph@users.noreply.github.com>
2021-05-24 20:49:35 +0300

    R

--- Question5.R
+++ Question5.R
@@ -2,12 +2,13 @@
 
 ###The objective of this analysis is to find the number of PLS components that yields the optimal R2 value. PLS models with 1 through 10 components were each evaluated using five repeats of 10-fold cross-validation
 
-PLS finds components that summarize the variation of the predictors while simultaneously
-requiring these components to have maximum correlation with the response. PLS is a compromise between the objectives of predictor space dimension reduction and a predictive relationship with the response. PLS can be viewed as a supervised dimension reduction procedure;
-PCR is an unsupervised procedure	
+#PLS finds components that summarize the variation of the predictors while simultaneously
+#requiring these components to have maximum correlation with the response. PLS is a compromise between the objectives of predictor space dimension reduction and a predictive relationship with the response. PLS can be viewed as a supervised dimension reduction procedure;
+#PCR is an unsupervised procedure	
 
-if 10-fold cross-validation was repeated five times, 50 different held-out sets would be
-used to estimate model efficacy
+#if 10-fold cross-validation was repeated five times, 50 different held-out sets would be
+#used to estimate model efficacy
+
 ```{r}
 library(AppliedPredictiveModeling)
 data(ChemicalManufacturingProcess)
@@ -16,7 +17,7 @@ data(ChemicalManufacturingProcess)
 
 
 ###a.	Using the "one-standard error" method, what number of PLS components provides the most parsimonious model?
-
+##(b),(c),(d)
 if two predictors are highly correlated, this implies that they are measuring the same underlying information. 
 Removing one should not compromise the performance of the model and might lead to a more precise and interpretable model.
 
@@ -59,52 +60,3 @@ b;b1 ## compare fits
 ```
 
 
-
-####b.	Compute the tolerance values for this example. If a 10 % loss in R2 is acceptable, then what is the optimal number of PLS components?
-```{r}
-
-
-##CODE
-
-
-
-
-
-
-```
-###c.	Several other models with varying degrees of complexity were trained and tuned and the results are presented in Figure below. 
-If the goal is to select the model that optimizes R2, then which model(s) would you choose, and why?
-  
-  ```{r}
-###CODE
-
-
-install.packages("pls")
-library(pls)
-library(caret)
-
-
-
-
-```
-
-
-d.	Prediction time, as well as model complexity are other factors to consider when selecting the optimal model(s).
-Given each model's prediction time, model complexity, and R2 estimates, which model(s) would you choose, and why?
-  
-  ```{r}
-
-###CODE
-
-
-
-
-
-```
-
-library(AppliedPredictiveModeling)
-data(ChemicalManufacturingProcess)
-library(caret)
-chemical.df <- data.frame(ChemicalManufacturingProcess)
-chemical.ZeroVar <- nearZeroVar(fingerprints.df, names = TRUE)
-predictors <- dim(chemical.df)[2] - length(chemical.ZeroVar)
